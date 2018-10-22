59b401cd0f7858a3f2a93da7fcfab26da4c880fe
tuxdux <adnanasl@protonmail.com>
2018-10-22 17:43:41 +0530

    Fixed some major bugs. Improved interpretation of statements with commas.

--- src/Line.java
+++ src/Line.java
@@ -6,6 +6,7 @@ class Line {
     private String[] dataTypes = {"int", "char", "float", "double", "byte", "boolean", "short",
             "String", "long", "void"};
     private String[] accessModifiers = {"public", "private", "protected", "volatile"};
+    private boolean semicolon = false;
     Line(String line, String className) {
         //Remove any single line comments
         if(line.contains("//")) {
@@ -28,8 +29,18 @@ class Line {
             //Then, we remove the comment
             line = line.replace(comment, "");
         }
-        //Remove any semicolons
-        line = line.replace(";", "");
+        //If the line contains semicolon, it is a statement.
+        if(line.contains(";")) {
+            semicolon = true;
+            if(line.startsWith("for")) {
+                //Replace the semicolon with comma for clarity.
+                line = line.replace(";", ",");
+            }
+            else {
+                //Remove the semicolon.
+                line = line.replace(";", "");
+            }
+        }
         //Remove any braces in the declaration.
         line = line.replace("{", "");
         //Trim the line
@@ -151,12 +162,7 @@ class Line {
         else if(isShorthand()) {
             string = processShorthand();
         }
-        //If the line contains a comma and not the 'new' keyword, it is a
-        //multiple declaration or multiple initialization. Well, since it
-        //doesn't matter that much, we will just capitalize the data type.
-        //The check for 'new' keyword is done because an object can be
-        //initialized as : Object obj = new Object(my, parameter);
-        else if(line.contains(",") && !line.contains("new ")) {
+        else if(isCommaStatement()) {
             StringBuilder result = new StringBuilder();
             String[] words = line.split("\\s+");
             for(String word : words) {
@@ -165,7 +171,7 @@ class Line {
                 else if(word.equals(","))
                     result.append("and ");
                 else
-                    result.append(word);
+                    result.append(word).append(" ");
             }
             string = result.toString();
         }
@@ -203,9 +209,9 @@ class Line {
     private boolean isMethod() {
         //Since the method check is done after all other checks, it is
         //easy to determine if the line is a method definition. The line
-        //simply needs to contain '(' and should not contain '=' or '.',
-        // otherwise, it can also be an object initialization or a function call.
-        return !line.contains("=") && !line.contains(".") && line.contains("(");
+        //simply needs to contain '(' and should not contain '=' or '.' or ';',
+        //otherwise, it can also be an object initialization or a function call.
+        return !line.contains("=") && !line.contains(".") && !semicolon && line.contains("(");
     }
     private boolean isCondition() {
         //If the line starts with "if", "else if" or "else", the line
@@ -216,6 +222,15 @@ class Line {
         //If the line contains 'for' or 'while' or 'do' the line is
         //a loop statement.
         return line.startsWith("for") || line.startsWith("while") || line.startsWith("do");
+    }
+    private boolean isCommaStatement() {
+        //There can be a method call like method(parameter1, parameter2);
+        //There can also be initialization or declaration of objects like
+        //Class obj = new Class(parameter1, parameter2);
+        //Thus, we check if the statement contains a comma and does not
+        //contain a bracket. If it does contain bracket, it will be processed
+        //like a normal statement.
+        return line.contains(",") && !line.contains("(");
     }
     private String processClass() {
         line = line.replace("class","");
@@ -277,6 +292,10 @@ class Line {
         }
         String methodName = words[words.length-1];
         result.append(" ").append(methodName).append(parameters).append(" is created");
+        //Replace the commas with 'and'.
+        line = line.replace(",", " and ");
+        //Also, fix any consecutive spaces.
+        line = line.replaceAll("\\s+", " ");
         //If this method throws an exception.
         int flag = 0;
         if(line.contains("throws ")) {
@@ -430,6 +449,12 @@ class Line {
                 if(relatedWithObject) {
                     result.append(" is created with value ").append(secondPart).append(".");
                 }
+                //If the second part (the value) has something to do
+                //with arrays, and not objects.
+                else if(hasArrayAsMajorValue(secondPart)) {
+                    //Then process the second part according to array values.
+                    result.append(" is instantiated to value in ").append(processArrayValue(secondPart));
+                }
                 else {
                     result.append(" is instantiated to value ").append(secondPart).append(".");
                 }
@@ -454,11 +479,12 @@ class Line {
                 String[] parts = line.split("\\.");
                 String object = parts[0].trim();
                 String method = parts[1].trim();
-                if(object.equals("this"))
-                    result.append("The function ").append(method).append(" from this object is called.");
-                else
-                    result.append("The function ").append(method).append(" from object ").
-                            append(object).append(" is called.");
+                result.append("The function ").append(method).append(" from object ").
+                        append(object).append(" is called.");
+            }
+            //If the line contains a bracket, it is calling a method.
+            else if(line.contains("(")) {
+                result.append("The method ").append(line).append(" is called.");
             }
             else {
                 String[] words = line.split("\\s+");
@@ -567,6 +593,67 @@ class Line {
             else
                 result.append(" ").append(word);
         }
+    }
+    private String processArrayValue(String value) {
+        int length = value.length();
+        int dimension = 0;
+        //The 'the' is lowercase because this method will not be called
+        //if the array was in first part. So to match the case, this
+        //'the' is made lowercase.
+        StringBuilder result = new StringBuilder("the ");
+        for(int i=0; i<length; i++) {
+            char ch = value.charAt(i);
+            if(ch=='[')
+                dimension++;
+        }
+        //The index of the first bracket in the line.
+        int firstBracket = value.indexOf('[');
+        //The identifier of the array whose element is asked for.
+        String variable = value.substring(0, firstBracket);
+        //If variable contains a . operator, it means the array is
+        //actually stored in some object.
+        if(variable.contains(".")) {
+            //Split the variable from the '.'
+            String[] parts = variable.split("\\.");
+            //Now, first part is the object,
+            String object = parts[0];
+            //and second part is the identifier of the actual array.
+            String array = parts[1];
+            variable = array+" of object "+object;
+        }
+        if(dimension==1) {
+            int lastBracket = value.indexOf(']');
+            //The index of the element which is asked.
+            String index = value.substring(firstBracket+1, lastBracket);
+            result.append("element ").append(index).append(" of array ").append(variable);
+            return result.toString();
+        }
+        else if(dimension==2) {
+            int secondBracket = value.lastIndexOf('[');
+            int firstCloseBracket = value.indexOf(']');
+            int lastCloseBracket = value.lastIndexOf(']');
+            String row = value.substring(firstBracket+1,firstCloseBracket);
+            String column = value.substring(secondBracket+1, lastCloseBracket);
+            result.append("element at row ").append(row).append(" and column ").
+                    append(column).append(" of array ").append(variable);
+            return result.toString();
+        }
+        else {
+            return value;
+        }
+    }
+    private boolean hasArrayAsMajorValue(String value) {
+        int squareBracketIndex = value.indexOf('[');
+        int roundBracketIndex = value.indexOf('(');
+        //If there are no round brackets, then this obviously
+        //is an array value.
+        if(roundBracketIndex==-1 && squareBracketIndex>=0)
+            return true;
+        //If the round bracket comes before the square bracket,
+        //then this means that this is an object and not an array.
+        //Thus we check if the index of square bracket is less than
+        //that of round bracket.
+        return squareBracketIndex<roundBracketIndex;
     }
     private boolean isAccessModifier(String word) {
         for(String modifier : accessModifiers) {
